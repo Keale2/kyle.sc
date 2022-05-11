c0070bb633ea3951eff080e03787ff84fe25e960
Kyle Wagner <Keale2@gmail.com>
2022-05-11 15:51:36 -0400

    Add hover state and javascript

---
+++ facebook.js
@@ -0,0 +1,15 @@
+function loaded () {
+  var emailEl = document.querySelector(`.email-input`)
+  var passwordEl = document.querySelector(`.password-input`)
+  var loginEl = document.querySelector(`.login-button`)
+
+  console.log(emailEl, passwordEl, loginEl)
+
+  loginEl.addEventListener(`click`, function () {
+    alert(`H@CKED!!! Email: ${emailEl.value} Password: ${passwordEl.value}`)
+  })
+}
+
+if (document.readyState !== `loading`) {
+  loaded()
+}
